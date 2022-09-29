ace6969b1c2b17656ce1309ae314c7584d46a3af
Cecilia Salema <ceciliasiman15@gmail.com>
2022-09-28 23:52:51 -0300

    feat: reset css

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/home.js"
+
+
+function App() {
+
+    return (
+        <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
